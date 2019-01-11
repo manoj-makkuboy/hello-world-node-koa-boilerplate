874f8f353950554fbaec317f2a3f326273d9871f
manoj-makkuboy <jesuismanoj@gmail.com>
2019-01-11 14:52:11 +0530

    Add sqlite integration

--- src/server.js
+++ src/server.js
@@ -2,6 +2,9 @@
 const Koa = require("koa");
 const router = require("koa-router")();
 const mount = require("koa-mount");
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./helloWorld.sqlite');
+console.log('Database helloWorld.sqlite created in project home directory')
 // create an instance of the Koa object
 const app = new Koa();
 // mount the route
@@ -11,3 +14,19 @@ if (require.main === module) {
   app.listen(3000); // default
   console.log('Access sample endpoint by http://localhost:3000/helloWorld');
 }
+
+db.serialize(function() {
+  db.run("CREATE TABLE lorem (info TEXT)");
+ 
+  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+  for (var i = 0; i < 10; i++) {
+      stmt.run("Ipsum " + i);
+  }
+  stmt.finalize();
+ 
+  db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+      console.log(row.id + ": " + row.info);
+  });
+});
+ 
+db.close();
